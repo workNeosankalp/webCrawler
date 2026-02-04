ac61b521f85f414a21739266f54c751b17352354
Dhruv Pal <dhruvpal.0905@gmail.com>
2026-02-04 18:07:12 +0530

    fix 6 for prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://webcrawler-8ybq.onrender.com"
+    "https://webcrawler-8ybq.onrender.com",
+    "https://web-crawler-ten.vercel.app/"
   ],
   credentials: true,
   optionsSuccessStatus: 200,
